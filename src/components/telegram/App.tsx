55b1802b5ca3933e06f8cfac69dce243d8d46432
llamiib0t <max774433@gmail.com>
2024-10-21 16:43:42 +1000

    set header color

--- src/components/telegram/App.tsx
+++ src/components/telegram/App.tsx
@@ -2,10 +2,17 @@ import { miniApp, useLaunchParams, useSignal } from '@telegram-apps/sdk-react';
 import { AppRoot } from '@telegram-apps/telegram-ui';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '../../router/router';
+import WebApp from '@twa-dev/sdk';
 
 export function App() {
     const lp = useLaunchParams();
     const isDark = useSignal(miniApp.isDark);
+
+    if (isDark) {
+        WebApp.setHeaderColor('#343437');
+    } else {
+        WebApp.setHeaderColor('#ededed');
+    }
 
     return (
         <AppRoot
