a035d6ab0a210c5a2fe86abda786136d7c0f9e20
llamiib0t <max774433@gmail.com>
2024-10-23 14:10:40 +1000

    log envs

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -4,6 +4,9 @@ import { Database } from './database.types';
 const url = import.meta.env.VITE_SUPABASE_PROJECT_URL;
 const api_key = import.meta.env.VITE_SUPABASE_API_KEY;
 
+console.log('VITE_SUPABASE_PROJECT_URL:', process.env.VITE_SUPABASE_PROJECT_URL);
+console.log('VITE_SUPABASE_API_KEY:', process.env.VITE_SUPABASE_API_KEY);
+
 console.log(url, api_key);
 
 // Create a single supabase client for interacting with your database
